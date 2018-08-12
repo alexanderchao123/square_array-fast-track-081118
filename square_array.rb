2d53a5284aa6dae620e974e22b1484f5c6294d3d
alexanderchao123 <chao.alexander.123@gmail.com>
2018-08-12 18:29:04 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,15 @@
-# def square_array(array)
-#   squared = []
-#
-#   array.each do |num|
-#     squared_num = num * num
-#     squared << squared_num
-#   end
-#
-#   squared
-# end
-
-
 def square_array(array)
-  array.map {|num| num * num}
+  squared = []
+
+  array.each do |num|
+    squared_num = num * num
+    squared << squared_num
+  end
+
+  squared
 end
+
+
+# def square_array(array)
+#   array.map {|num| num * num}
+# end
