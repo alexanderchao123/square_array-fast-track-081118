3c72733dbe215120d8d7e2f0f6866e308557987c
alexanderchao123 <chao.alexander.123@gmail.com>
2018-08-12 18:28:19 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,13 @@
-def square_array(array)
-  squared = []
-
-  array.each do |num|
-    squared_num = num * num
-    squared << squared_num
-  end
-
-  squared
-end
+# def square_array(array)
+#   squared = []
+#
+#   array.each do |num|
+#     squared_num = num * num
+#     squared << squared_num
+#   end
+#
+#   squared
+# end
 
 
 def square_array(array)
